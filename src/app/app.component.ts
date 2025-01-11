14610b1db3a01221ea0a68d0cffe75da77e72093
Gustavo Cremonez <gustavocremonezgc@gmail.com>
2025-01-11 08:15:40 -0300

    chore(layout): creating and styling side-bar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideBarComponent } from './core/layouts/side-bar/side-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'InventifyApp';
